fcb57379046b5193dbf220956863bd8198db85de
Ansh Tripathi <anshtripathi2022@vitbhopal.ac.in>
2025-12-23 22:03:11 +0530

    07: adding ai providers

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,29 +1,42 @@
 import prisma from "@/lib/db";
 import { inngest } from "./client";
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { generateText } from "ai";
+//import { createOpenAI } from "@ai-sdk/openai"
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world"},
-  { event: "test/hello.world" },
+const google = createGoogleGenerativeAI();
+//const openai = createOpenAI();
+
+export const execute = inngest.createFunction(
+  { id: "execute-ai"},
+  { event: "execute/ai" },
   async ({ event, step }) => {
+    await step.sleep("pretend" , "5s"); 
 
-    // Fetching the video
-    await step.sleep("fetching", "5s");
+    const { steps: geminiSteps } = await step.ai.wrap(
+      "gemini-generate-text" ,
+      generateText, 
+      {
+      model: google("gemini-2.5-flash"),
+      system: "You are a helpful assistant.",
+      prompt: "What is 2 + 2?",
+      } 
+    );
+    
+    /*  USE IF YOU HAVE OPENAI KEY AND HAVE MINIMUM 5$ / 447INR in your openai  */
+    //  const { steps: openaiSteps } = await step.ai.wrap(
+    //   "openai-generate-text" ,
+    //   generateText, 
+    //   {
+    //   model: openai("gpt-5-chat-latest"),
+    //   system: "You are a helpful assistant.",
+    //   prompt: "What is 2 + 2?",
+    //   } 
+    // ); 
 
-    // Transcribing
-    await step.sleep("transcribing", "5s");
-
-    // Sending transcription to AI
-    await step.sleep("sending-to-ai", "5s");
-
-    await step.run("create-workflow" , () => {
-        return prisma.workflow.create({
-            data: {
-                name: "workflow-from-inngest",
-            },
-        });
-    });
-
-
-    return { message: `Hello ${event.data.email}!` };
+    return {
+      geminiSteps,
+      //openaiSteps,
+    };
   },
 );
